741ac00f148c6f236d645d9458bfc33c9cbbaf89
joseluisgs <joseluisgonsan@gmail.com>
2020-07-13 18:58:43 +0200

    Middleware de rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import EntryList from '../views/EntryList.vue';
-import Registro from '../views/Registro.vue';
-import Login from '../views/Login.vue';
-import Recuperar from '../views/Recuperar.vue';
+import AuthService from '@/services/AuthService';
+import EntryList from '@/views/EntryList.vue';
+import Registro from '@/views/Registro.vue';
+import Login from '@/views/Login.vue';
+import Recuperar from '@/views/Recuperar.vue';
+import Perfil from '@/views/Perfil.vue';
 
 Vue.use(VueRouter);
 
@@ -22,16 +24,25 @@ const routes = [
     path: '/registro',
     name: 'registro',
     component: Registro,
+    meta: { publico: true },
   },
   {
     path: '/login',
     name: 'login',
     component: Login,
+    meta: { publico: true },
   },
   {
     path: '/recuperar',
     name: 'recuperar',
     component: Recuperar,
+    meta: { publico: true },
+  },
+  {
+    path: '/perfil',
+    name: 'perfil',
+    component: Perfil,
+    meta: { privado: true },
   },
 ];
 
@@ -39,6 +50,27 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+// Filtros de Middleware
+router.beforeEach((to, from, next) => {
+  const privado = to.matched.some((destino) => destino.meta.privado);
+  const publico = to.matched.some((destino) => destino.meta.publico);
+  const usuarioActual = AuthService.getUsuarioActivo();
+
+  if (privado && !usuarioActual) {
+    console.info('Se necesita autentificación');
+    next({ name: 'login' });
+  } else if (publico && usuarioActual) {
+    console.info('Ruta para usuarios anónimos');
+    next({ name: 'portada' });
+  } else if (privado && usuarioActual) {
+    console.info('Autentificación establecida');
+    next(); // Lo dejamos pasar
+  } else {
+    console.info('Cualquiera de las otras combinaciones posibles');
+    next();
+  }
 });
 
 export default router;
---
+++ src/views/Perfil.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="section">
+    <br />
+    <br />
+    <h1>Perfil de usuario</h1>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+section {
+  background-color: lightgray;
+  flex-grow: 1;
+  padding: 1rem;
+  padding-top: 100px;
+  padding-bottom: 60px;
+}
+</style>
